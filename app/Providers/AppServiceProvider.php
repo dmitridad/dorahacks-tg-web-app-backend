0706c25ecde6323db67b72911a5be2307780e1b9
Dmitry Lyubetsky <liubetskii.dmitrii@gmail.com>
2024-06-17 21:28:09 +0400

    feat: bind and configure new services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Services\TON\SmartContracts\Game;
+use App\Services\TON\SmartContracts\GameInterface;
+use App\Services\TON\TonCenterHttpGateway;
+use App\Services\TON\TonHttpGatewayInterface;
+use Http\Client\Common\HttpMethodsClient;
+use Http\Client\Common\HttpMethodsClientInterface;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Http\Discovery\Psr18ClientDiscovery;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +19,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TonHttpGatewayInterface::class, TonCenterHttpGateway::class);
+        $this->app->when(TonCenterHttpGateway::class)
+            ->needs(HttpMethodsClientInterface::class)
+            ->give(function () {
+                // TODO check search process
+                return new HttpMethodsClient(
+                    Psr18ClientDiscovery::find(),
+                    Psr17FactoryDiscovery::findRequestFactory(),
+                    Psr17FactoryDiscovery::findStreamFactory(),
+                );
+            });
+        $this->app->when(TonCenterHttpGateway::class)
+            ->needs('$tonCenterApiKey')
+            ->give(env('TON_CENTER_API_KEY'));
+        $this->app->when(TonCenterHttpGateway::class)
+            ->needs('$isMainNet')
+            ->give(env('USE_TON_MAIN_NET'));
+
+        $this->app->bind(GameInterface::class, Game::class);
     }
 
     /**
