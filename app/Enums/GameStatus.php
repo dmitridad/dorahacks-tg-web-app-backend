9e0703dc601993a263dda8de0aab1f6f3c85b260
Dmitry Lyubetsky <liubetskii.dmitrii@gmail.com>
2024-06-17 21:25:18 +0400

    feat: upgrade GameStatus enum

--- app/Enums/GameStatus.php
+++ app/Enums/GameStatus.php
@@ -7,4 +7,12 @@ enum GameStatus: string
     case Created = 'created';
     case Started = 'started';
     case Finished = 'finished';
+
+    public function canJoin(): bool
+    {
+        return match($this) {
+            GameStatus::Created => true,
+            GameStatus::Started, GameStatus::Finished => false,
+        };
+    }
 }
