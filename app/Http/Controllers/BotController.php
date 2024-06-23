a8bac1c3b5f733b8bff6af015ccb34350755872c
Dmitry Lyubetsky <liubetskii.dmitrii@gmail.com>
2024-06-23 16:33:33 +0400

    fix: fix bot webhook handler v2

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -4,35 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class BotController extends Controller
 {
+    /**
+     * @throws \Exception
+     */
     public function webhook(Request $request)
     {
         $message = $request->post('message');
-
         $botToken = env('TG_BOT_TOKEN');
-        $chatId = $message['chat']['id'];
-        $messageText = 'Enjoy your game';
-        $buttonText = "Play";
-        $buttonUrl = env('TG_WEB_APP_URL');
-
-        $url = env('TG_API_BASE_URL')."/bot$botToken/sendMessage";
+        $sendMessageUrl = env('TG_API_BASE_URL')."/bot$botToken/sendMessage";
 
         $data = [
-            'chat_id' => $chatId,
-            'text' => $messageText,
+            'chat_id' => $message['chat']['id'],
+            'text' => env('TG_WEB_APP_PLAY_MSG'),
             'reply_markup' => json_encode([
                 'inline_keyboard' => [
                     [
-                        ['text' => $buttonText, 'url' => $buttonUrl]
-                    ]
-                ]
+                        [
+                            'text' => env('TG_WEB_APP_PLAY_BTN_MSG'),
+                            'web_app' => ['url' => env('TG_WEB_APP_URL')]
+                        ],
+                    ],
+                ],
             ])
         ];
 
-        // TODO handle
-        $response = Http::post($url, $data);
+        $response = Http::post($sendMessageUrl, $data);
+        $responseJson = $response->json();
+        if (!$responseJson['ok']) {
+            $errorMsg = 'tg sendMessage error';
+            Log::error($errorMsg, [
+                'data' => $data,
+                'url' => $sendMessageUrl,
+            ]);
+
+            throw new \Exception($errorMsg);
+        }
 
         return response()->noContent();
     }
